2f739b0f03034689ae2cb78ea3c8e39efad641a5
cy-bug <shashwat5590@gmail.com>
2023-01-31 14:41:37 +0530

    a

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark';
 const lottieElms = $('.lottie-elm');
 const lottieInstances = {};
+var isNavAnimating = false;
 
 lottieElms.each(function () {
     const lottieElm = $(this);
@@ -28,19 +29,22 @@ lottieElms.each(function () {
 var menuToggled = true;
 var menuBtn = $("#menu-btn");
 menuBtn.click(function () {
+    if (isNavAnimating) return;
+    isNavAnimating = true;
     const menuBtnAnim = lottieInstances['hamburger'];
     if (menuToggled) {
+        isNavAnimating = true;
         menuBtnAnim.playSegments([0, 39], true);
         menuToggled = false;
 
         $(".menu").css("display", "flex");
-
         gsap.to(".menu", {
             duration: 0.5,
-            height: "100vh",
+            opacity: "1",
             ease: "power4.out",
             onComplete: function () {
                 $(".menu").addClass("menu-open");
+                gsap.to(".menu-container", { opacity: 1, duration: 0.5, onComplete: function () {isNavAnimating = false;}});
             },
         });
         
@@ -53,7 +57,7 @@ menuBtn.click(function () {
             }
         });
         
-
+        
     } else {
         menuBtnAnim.playSegments([39, 78], true);
         menuToggled = true;
@@ -68,11 +72,16 @@ menuBtn.click(function () {
                 $(".nav .landing-text").removeClass("no-bg");
             },
         });
+        gsap.to(".menu-container", { opacity: 0, duration: 0.5 });
         gsap.to(".menu", {
             delay: 0.5,
             duration: 1,
-            height: "0",
-            ease: "power4.out"
+            opacity: 0,
+            ease: "power4.out",
+            onComplete: function () {
+                $(".menu").css("display", "none");
+                isNavAnimating = false;
+            },
         });
     }
 });
